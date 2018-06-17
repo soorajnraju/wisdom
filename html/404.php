1860fb9d7900433b035c499bf150b77b7956fd4a
Sooraj <soorajnraju@gmail.com>
2018-06-17 10:14:53 +0530

    404

--- html/404.php
+++ html/404.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Wisdom-Page1</title>
+        <title>Wisdom-404</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <?php include HOME . 'html/header.php'; ?>
